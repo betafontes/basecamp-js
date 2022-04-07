64c3ac03ba62cb56e6e7d9602546e6656cf7dfb6
betafontes <robertafontesds@gmail.com>
2022-04-07 00:03:36 -0300

    new commit

---
+++ modulos/exercises/main.mjs
@@ -0,0 +1,5 @@
+import {mostraCidade, mostraIdade, mostraHobby} from "./funcoes.mjs";
+
+console.log(mostraIdade('Roberta', 19));
+console.log(mostraCidade('Roberta', 'Rio de Janeiro'));
+console.log(mostraHobby('Roberta', 'jogar'));
